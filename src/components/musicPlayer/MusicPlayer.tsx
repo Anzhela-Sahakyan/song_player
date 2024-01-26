072d00fd4214efd372b8eaa172c03cb137e36a91
Anzhela <sahakyananzhela@gmail.com>
2024-01-26 13:12:24 +0400

    adding comments for MusicPlayer component

--- src/components/musicPlayer/MusicPlayer.tsx
+++ src/components/musicPlayer/MusicPlayer.tsx
@@ -13,13 +13,24 @@ enum SortMode {
 }
 
 export default function MusicPlayer() {
-  // Todo fully explain the meaning of each piece of state
+  // State for searching, it is initialized as an empty string.
   const [searchQuery, setSearchQuery] = useState("");
+
+  // State for sorting, it is initialized with ascending order.
   const [sort, setSort] = useState(ESortOption.TrackNumberAsc);
+
+  // State for displaying and updating the list of songs. It is initialized with the imported songs data
   const [songList, setSongList] = useState(songs);
+
+  // State for tracking the current sorting mode. It is initialized as AutoSort, menaing being sorted with the button and not drag-and-drop.
   const [sortMode, setSortMode] = useState(SortMode.AutoSort);
 
-  // Todo explain the logic of sortin and filtering
+  // useMemo hook is used to optimize the performance and avoid unnecessary re-renderings.
+  // Inside useMemo an imported filterSongList function is called which is used to filter the song list based on
+  // the search query and sort the list based on the selected sort option. The default sorting is in ascending order.
+  // Filtering with searchQuery is done by song name or artist name.
+  // A dependencies array is provided at the end of useMemo  hook, so that the it re-renders the component when any item
+  // dependencies array is changed.
   const filteredSongList = useMemo(
     () =>
       filterSongList({
